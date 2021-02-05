3d34c24d74abf6b332528edfec9b914c851a4da5
Frank Ngeno <frankngeno@gmail.com>
2021-02-05 09:35:51 -0500

    commit changes

--- src/main/java/com/grl/hibernateDemo/pickOrderHandler/PickOrderNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.grl.hibernateDemo.pickOrderHandler;
-
-public class PickOrderNotFoundException extends RuntimeException {
-    public PickOrderNotFoundException(Long id) {
-        super("Could not find new pick orders " + id);
-    }
-}
