88b852a4f7790a7a9b7c22c83d147270e0bb01e1
Frank Ngeno <frankngeno@gmail.com>
2021-02-06 14:19:03 -0500

    changes on the app

--- src/test/java/com/grl/hibernateDemo/HibernateDemoApplicationTests.java
+++ src/test/java/com/grl/hibernateDemo/HibernateDemoApplicationTests.java
@@ -1,14 +1,13 @@
 package com.grl.hibernateDemo;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HibernateDemoApplicationTests {
 
-	@Test
-	public void pickOrdersTest() {
-
-	}
+//	@Test
+//	public void pickOrdersTest() {
+//
+//	}
 
 }
